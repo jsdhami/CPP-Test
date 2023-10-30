bce24b7f24a1b2473be306b1d9eac38c62c7fc28
Janak singh dhami <info@jsdhami.com.np>
2023-10-30 16:38:30 +0545

    some

--- CPP Basic/main.cpp
+++ CPP Basic/main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a = 5;
-    int b = 6;
-    int sum = a + b;
-    cout << "The sum of a and b is: " << sum << endl;
-    cin >> a;
-    cout << "You entered: " << a << endl;
+   cout << "Hello World!" << endl ;
     return 0;
 }
