2afa0c6d5b5b510d4201e1373ab6861cca3194c1
Janak singh dhami <info@jsdhami.com.np>
2023-11-09 20:13:54 +0545

    add loops

---
+++ LOOPS/ifelseif.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    if (a > 5 )
+    {
+        cout << "a is greater than 5" << endl;
+    }else if (a < 5)
+    {
+        cout << "a is less than 5" << endl;
+    }else
+    {
+        cout << "a is equal to 5" << endl;
+    }
+    string name = "Janak";
+    if (name == "Janak")
+    {
+        cout << "Name is Janak" << endl;
+    }else if (name == "Raj")
+    {
+        cout << "Name is Raj" << endl;
+        }else
+        {
+            cout << "Name is not Janak or Raj";
+        }
+        
+
+    return 0;
+}
